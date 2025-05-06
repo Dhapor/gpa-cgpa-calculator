0bf3a1c6276a64979ee20bc8c0125e91a76c3ec0
Oludare Samuel <138253128+Dhapor@users.noreply.github.com>
2025-05-06 15:54:34 +0100

    Update calc.py

--- calc.py
+++ calc.py
@@ -75,7 +75,7 @@ elif menu == "4.0 GPA/CGPA Calculator":
     sessions = st.number_input("How many sessions?", min_value=1, step=1)
 
     for s in range(1, sessions + 1):
-        st.subheader(f"📘 Session {s}")
+        st.markdown(f"📘 Session {s}")
         semesters = st.number_input(f"How many semesters in session {s}?", min_value=1, step=1, key=f"sem_{s}")
 
         for sem in range(1, semesters + 1):
@@ -178,7 +178,7 @@ elif menu == "5.0 GPA/CGPA Calculator":
     sessions = st.number_input("How many sessions?", min_value=1, step=1)
 
     for s in range(1, sessions + 1):
-        st.subheader(f"📘 Session {s}")
+        st.markdown(f"📘 Session {s}")
         semesters = st.number_input(f"How many semesters in session {s}?", min_value=1, step=1, key=f"sem_{s}")
 
         for sem in range(1, semesters + 1):
